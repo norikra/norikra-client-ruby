1fa646764824282ea619dea274badd337b72f13c
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-05-14 22:12:00 +0900

    writing.

--- lib/norikra/client/cli.rb
+++ lib/norikra/client/cli.rb
@@ -1,8 +1,8 @@
 require 'thor'
 require 'norikra/client'
 
-require 'norikra/cli/parser'
-require 'norikra/cli/formatter'
+require 'norikra/client/cli/parser'
+require 'norikra/client/cli/formatter'
 
 class Norikra::Client
   module CLIUtil
@@ -15,11 +15,25 @@ class Norikra::Client
     include Norikra::Client::CLIUtil
 
     desc "list", "show list of tables"
-    option :simple, :type => :boolean, :default => false, :desc => "suppress count of tables", :aliases => "-s"
+    option :simple, :type => :boolean, :default => false, :desc => "suppress header/footer", :aliases => "-s"
     def list
+      puts "TABLENAME" unless options[:simple]
       tables = client(parent_options).tables
-      p tables.join("\n")
-      p "#{tables.size} tables found." unless options[:s]
+      tables.each do |t|
+        puts t
+      end
+      puts "#{tables.size} tables found." unless options[:simple]
+    end
+  end
+
+  class Typedef < Thor
+    include Norikra::Client::CLIUtil
+
+    desc "list", "show list of field definitions"
+    def list
+      defs = client(parent_options).typedefs
+      require 'pp'
+      pp defs
     end
   end
 
@@ -27,14 +41,14 @@ class Norikra::Client
     include Norikra::Client::CLIUtil
 
     desc "list", "show list of queries"
-    option :simple, :type => :boolean, :default => false, :desc => "suppress count of queries", :aliases => "-s"
+    option :simple, :type => :boolean, :default => false, :desc => "suppress header/footer", :aliases => "-s"
     def list
-      puts "TABLES\tQUERY_NAME\tQUERY"
+      puts "TABLES\tQUERY_NAME\tQUERY" unless options[:simple]
       queries = client(parent_options).queries
       queries.sort{|a,b| (a['tablename'] <=> b['tablename']).nonzero? || a['name'] <=> b['name']}.each do |q|
         puts "#{q['tablename']}\t#{q['name']}\t#{q['expression']}"
       end
-      puts "#{queries.size} queries found." unless options[:s]
+      puts "#{queries.size} queries found." unless options[:simple]
     end
 
     desc "add TABLE_NAME QUERY_NAME QUERY_EXPRESSION", "register a query"
@@ -48,12 +62,19 @@ class Norikra::Client
 
     desc "send TABLE_NAME", "send data into table"
     option :format, :type => :string, :default => 'json', :desc => "format of input data per line of stdin [json(default), ltsv]"
+    option :batch_size, :type => :numeric, :default => 10000, :desc => "records sent in once transferring (default: 10000)"
     def send(tablename)
       client = client(parent_options)
       parser = parser(options[:format])
+      buffer = []
       $stdin.each_line do |line|
-        client.send(tablename, parser.parse(line))
+        buffer.push(parser.parse(line))
+        if buffer.size >= options[:batch_size]
+          client.send(tablename, buffer)
+          buffer = []
+        end
       end
+      client.send(tablename, buffer) if buffer.size > 0
     end
 
     desc "fetch QUERY_NAME", "fetch events from specified query"
@@ -66,7 +87,7 @@ class Norikra::Client
 
       client(parent_options).event(query_name).each do |time,event|
         event = {options[:time_key] => Time.at(time).strftime(options[:time_format])}.merge(event)
-        puts formatter.fomat(event)
+        puts formatter.format(event)
       end
     end
 
@@ -83,7 +104,7 @@ class Norikra::Client
 
       data.keys.sort.each do |queryname|
         events = data[queryname]
-        events.each do |event|
+        events.each do |time,event|
           event = {
             options[:time_key] => Time.at(time).strftime(options[:time_format]),
             options[:query_name_key] => queryname,
@@ -102,6 +123,9 @@ class Norikra::Client
 
     desc "table CMD ...ARGS", "manage tables"
     subcommand "table", Table
+
+    desc "typedef CMD ...ARGS", "manage table field/datatype definitions"
+    subcommand "typedef", Typedef
 
     desc "query CMD ...ARGS", "manage queries"
     subcommand "query", Query
