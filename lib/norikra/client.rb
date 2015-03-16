8b70734f556b5bd2095f26ddac160a0e4548f021
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-03-16 15:19:52 +0900

    add ServiceUnavailableError for shutoff mode

--- lib/norikra/client.rb
+++ lib/norikra/client.rb
@@ -6,6 +6,7 @@ module Norikra
   module RPC
     class ClientError < MessagePack::RPCOverHTTP::RemoteError; end
     class ServerError < MessagePack::RPCOverHTTP::RemoteError; end
+    class ServiceUnavailableError < MessagePack::RPCOverHTTP::RemoteError; end
   end
 
   class Client
